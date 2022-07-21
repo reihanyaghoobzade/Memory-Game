e2a40c2990c3e6dae53dbefeeff0c8fe1267ea26
Reihane Yaghoobzade <reihanygh2002@gmail.com>
2022-07-21 17:10:55 +0430

    add vuex

---
+++ src/components/ConfigModal.vue
@@ -0,0 +1,41 @@
+<template>
+  <div
+    class="flex justify-center items-center bg-transparent rounded-xl absolute top-0 w-full h-full z-20"
+  >
+    <div class="container mx-auto">
+      <div class="w-3/5 mx-auto py-8 px-6 rounded-lg bg-stone-400">
+        <div class="flex flex-col justify-end items-center gap-8">
+          <p class="text-lg font-semibold">
+            تنظیمات را انجام نداده‌اید، با تنظیمات پیش‌فرض ادامه می‌دهید؟
+          </p>
+          <div class="flex justify-end items-center gap-8 w-full">
+            <button
+              @click="clickHandler"
+              class="py-2 px-6 lg:px-8 text-xl text-white bg-stone-700 rounded-xl"
+            >
+              ادامه
+            </button>
+            <button
+              @click="router.replace({ path: '/' })"
+              class="py-2 px-6 lg:px-8 text-xl text-white bg-stone-700 rounded-xl"
+            >
+              تغییر
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useRouter } from "vue-router";
+const router = useRouter();
+const emit = defineEmits();
+
+function clickHandler() {
+  emit("continue", false);
+}
+</script>
+
+<style></style>
